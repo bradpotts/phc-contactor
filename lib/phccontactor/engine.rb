dce5e1fb44cbe2d37867865d76550f3b1e389b70
BradPotts <brad.potts@phcnetworks.net>
2018-06-30 10:36:09 -0700

    fixes to engine updated gemspec

--- lib/phccontactor/engine.rb
+++ lib/phccontactor/engine.rb
@@ -57,10 +57,5 @@ module Phccontactor
       Phctitleseo::ApplicationController.helper(ApplicationHelper)
     end
 
-    # Load Language Files
-    config.before_initialize do
-      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    end
-
   end
 end
